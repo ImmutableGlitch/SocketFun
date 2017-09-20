1b9a5daf6cd0e8aefce21fc0463898d046e4de69
Christopher Caldwell <ccaldwell488@gmail.com>
2017-09-20 11:11:31 +0100

    Basic one way communication

---
+++ clientSocket/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace clientSocket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static TcpClient client = new TcpClient();
+        byte[] input = new byte[65536];
+        NetworkStream stream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender , RoutedEventArgs e)
+        {
+            box.Focus();
+            BeginConnection();
+        }
+
+        public void BeginConnection()
+        {
+            try
+            {
+                //client.Connect("192.168.0.44" , 8888);
+                client.Connect("82.7.141.89" , 8888);
+                stream = client.GetStream();
+                block.Text += ">>Connected to server!" + Environment.NewLine;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Could not connect to server!");
+                Close();
+            }
+        }
+
+        private void Btn_Click(object sender , RoutedEventArgs e)
+        {
+            byte[] output = Encoding.ASCII.GetBytes(box.Text + "$");
+            stream.Write(output , 0 , box.Text.Length + 1);
+            stream.Flush();
+
+            stream.Read(input,0,(int)client.ReceiveBufferSize);
+            string response = Encoding.Default.GetString(input);
+            block.Text += "You: " + box.Text + "\t" + response.Substring(0,response.LastIndexOf("$")) + Environment.NewLine;
+            box.Text = "";
+            box.Focus();
+        }
+    }
+}
---
+++ serverSocket/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace serverSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deprecated method
+            TcpListener server = new TcpListener(8888);
+            TcpClient client = default(TcpClient);
+
+            int requestCount = 0;
+            server.Start();
+            Console.WriteLine(">> Server Started");
+            client = server.AcceptTcpClient();
+            Console.WriteLine(">> Accepted client connection\n");
+            byte[] buffer = new byte[65536];
+
+            while (true)
+            {
+                try
+                {
+                    requestCount++;
+                    NetworkStream stream = client.GetStream();
+
+                    stream.Read(buffer , 0 , (int)client.ReceiveBufferSize);
+
+                    string data = Encoding.ASCII.GetString(buffer);
+                    data = data.Substring(0 , data.IndexOf("$"));
+                    Console.WriteLine("Client: " + data);
+
+                    string response = "(Recieved)$";
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(response);
+                    stream.Write(sendBytes , 0 , sendBytes.Length);
+
+                    stream.Flush();
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("\n\tERROR: {0}\n", ex.Message);
+                    client.Close();
+                    server.Stop();
+                    Console.WriteLine(">> EXITING...");
+                    Console.ReadLine();
+                    break;
+                }
+            }//while
+
+        }
+    }
+}
